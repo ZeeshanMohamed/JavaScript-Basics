725c202ef54076b1254dfc43e4fc3c96a0c8e52d
Zeeshan <zeeshanmd6975@gmail>
2025-03-07 16:12:39 +0530

    first commit

---
+++ main1.js
@@ -0,0 +1,46 @@
+// //primitive data types
+// //number string boolean null undefined symbole bigint
+
+// let a = 1;  
+// let b = "hello";
+// let c = true;
+// let d = null;
+// let e = undefined;
+// let f = Symbol("hello");
+// let g = BigInt(123);
+
+// //object
+
+// let h = {
+//   name: "hello",
+//   age: 12,
+//   isStudent: true
+// }
+
+// console.log(h.name);
+// console.log(h.age);
+
+//------------------------------------------------------
+
+// let a = "areeb"
+// let b = 9
+
+// console.log( a + b)
+// console.log(typeof a + b)
+
+
+//------------------------
+
+// let a1 = {
+//   name: "areeb",
+//   age: 23,
+//   isStudent: false
+// }
+
+// a1['gender'] = "male-kid"
+
+// console.log(a1)
+// console.log(a1.name)
+
+//---------------------------
+
---
+++ practice-1.js
@@ -0,0 +1,37 @@
+let a = "areeb"
+let b = 16
+console.log(a+b)
+
+//------------------------------
+
+console.log(typeof a, typeof b)
+
+//-----------------------------
+
+// const obj = {
+//     name: "areeb",
+//     age: 16
+// }
+
+// const obj = 12;
+// console.log(obj)
+
+//-------------------------------
+
+const obj = {
+         name: "areeb",
+        age: 16
+     }
+
+obj['marital status'] = 'single';
+console.log(obj);
+
+//------------------------------
+
+let a1 = {
+    mutable : 'changable',
+    immutable: 'unchangable',
+    Object: 'dictionary like in JS'
+}
+
+console.log(a1)
