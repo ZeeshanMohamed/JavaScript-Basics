60fb368c76c23ce3b89d4a68b6747371cdaf68c8
Zeeshan <zeeshanmd6975@gmail>
2025-03-13 15:19:34 +0530

    loop and function practice

---
+++ function.js
@@ -0,0 +1,19 @@
+// function onePlusAvg(x,y){
+//     return 1 + (x+y) /2
+
+// }
+
+// let a = 1;
+// let b= 2;
+// let c = 3;
+
+// console.log(onePlusAvg(a,b))
+
+
+//--------------------------
+
+const hello = ()=>{
+    console.log("Hello howre you")
+}
+
+hello()
---
+++ loopandfunctionprcatice.js
@@ -0,0 +1,30 @@
+let marks = {
+    'faheem': 99,
+    'areeb': 98,
+    'zeeshan': 97
+}
+
+// for ( let i=0; i < Object.keys(marks).length; i++){
+//     console.log(Object.keys(marks)[i] + " " + Object.values(marks)[i])
+// }
+
+// for (let i in marks){
+//     console.log(i + " " + marks[i])
+// }
+const prompt = require("prompt-sync")();
+// let num = 4;
+// let i 
+// while(i != num){   
+//     console.log('try again')
+//     i = prompt('enter correct number:')
+// }
+// console.log("you are right")
+
+
+
+// function
+const mean = (a,b,c,d,e)=>{
+    return (a+b+c+d+e)/5
+}
+// function mean(a,b,c,d,e){}
+console.log(mean(5,10,15,20,25))
