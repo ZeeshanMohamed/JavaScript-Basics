ac22ad0786a422b322a7db80ba9b912a5ac13004
Zeeshan <zeeshanmd6975@gmail>
2025-03-14 11:13:02 +0530

    stringpractice

---
+++ string.js
@@ -0,0 +1,15 @@
+let name = "zeeshan"
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2,5))
+// console.log(name.replace("zee"," "))
+
+let second = "mohamed"
+console.log(name.concat( second))
+
+let third = "       buuubuuu       "
+console.log(third.trim())
+for (let i in second){
+    console.log(second[i])
+}
---
+++ stringpractice.js
@@ -0,0 +1,20 @@
+// let a = "hell yeah"
+// console.log(a.length)
+
+
+// const sentence = "Areeb is sweet as jhangri"
+// const word = "sweet"
+// // console.log(`The word "${word}" ${sentence.includes(word) ? 'is there' : 'is not'} in the sentence.`);
+// console.log(word, sentence.includes(word) ? 'is there' : 'is not', 'in the sentence.')
+
+// let b = "banana"
+// console.log(b.toUpperCase())
+// console.log(b.toLowerCase())
+
+let sen = "please give Rs 1000"
+console.log(sen.slice(15))
+
+let d = 'faheem'
+console.log(d.replace('f', 's'))
+console.log(d)
+
